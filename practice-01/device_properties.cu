6a1d6bf083cf4d6ffafba9a24b6ad2b565e942f3
Bernardo <0253019@up.edu.mx>
2026-05-20 18:45:53 -0600

    Add safety-check to code

--- practice-01/device_properties.cu
+++ practice-01/device_properties.cu
@@ -4,9 +4,16 @@
 using namespace std;
 
 int main() {
-    int deviceCount;
+    // 1. Initialize to 0 so we don't read garbage
+    int deviceCount = 0; 
+    
+    // 2. Catch the actual error returned by CUDA
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
 
-    cudaGetDeviceCount(&deviceCount);
+    if (err != cudaSuccess) {
+        cout << "CUDA Error: " << cudaGetErrorString(err) << endl;
+        return 1;
+    }
 
     if (deviceCount == 0) {
         cout << "No CUDA GPU found." << endl;
@@ -14,25 +21,19 @@ int main() {
     }
 
     cudaDeviceProp prop;
+    err = cudaGetDeviceProperties(&prop, 0);
 
-    cudaGetDeviceProperties(&prop, 0);
+    if (err != cudaSuccess) {
+        cout << "Failed to get device properties: " << cudaGetErrorString(err) << endl;
+        return 1;
+    }
 
+    // If it gets here, the data is 100% real and safe to print!
     cout << "GPU Name: " << prop.name << endl;
     cout << "Multiprocessor count (MP): " << prop.multiProcessorCount << endl;
     cout << "Max blocks per Multiprocessor: " << prop.maxBlocksPerMultiProcessor << endl;
     cout << "Max threads per Multiprocessor: " << prop.maxThreadsPerMultiProcessor << endl;
     cout << "Max threads per block: " << prop.maxThreadsPerBlock << endl;
-
-    cout << "Max grid size:" << endl;
-    cout << "  X: " << prop.maxGridSize[0] << endl;
-    cout << "  Y: " << prop.maxGridSize[1] << endl;
-    cout << "  Z: " << prop.maxGridSize[2] << endl;
-
-    cout << "Max threads per block dimension:" << endl;
-    cout << "  X: " << prop.maxThreadsDim[0] << endl;
-    cout << "  Y: " << prop.maxThreadsDim[1] << endl;
-    cout << "  Z: " << prop.maxThreadsDim[2] << endl;
-
     cout << "Warp size: " << prop.warpSize << endl;
 
     return 0;
